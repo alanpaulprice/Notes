fa92786be329df65c2a405bbb9c1f28d09c788f3
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-05-10 16:39:11 +0100

    move MinimapButton stuff to separate file

---
+++ MinimapButton.lua
@@ -0,0 +1,28 @@
+local addonName, addon = ...
+addon.MinimapButton = {}
+local MinimapButton = addon.MinimapButton
+
+function MinimapButton:Initialize()
+	local minimapButton = LibStub("LibDataBroker-1.1"):NewDataObject(addonName, {
+		type = "data source",
+		text = addonName,
+		icon = "Interface\\ICONS\\INV_Misc_PaperBundle02a.blp", -- "Interface\\ICONS\\INV_Misc_Note_04.blp"
+		OnClick = function(_, button)
+			if button == "LeftButton" then
+				addon.MainFrame:Toggle()
+			elseif button == "RightButton" then
+				--TODO: OPEN CONFIG
+			end
+		end,
+		OnTooltipShow = function(tooltip)
+			if not tooltip or not tooltip.AddLine then
+				return
+			end
+
+			tooltip:AddLine(addonName)
+		end,
+	})
+
+	local icon = LibStub("LibDBIcon-1.0", true)
+	icon:Register(addonName, minimapButton, NotesDB)
+end
