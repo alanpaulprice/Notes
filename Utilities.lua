467a25e0770907aa88b82c4fbaff68ceddcca471
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-05-14 12:43:22 +0100

    Utilities.lua: add `GetTableKeys` and `PrintTableKeys` functions

--- Utilities.lua
+++ Utilities.lua
@@ -2,6 +2,24 @@ local addonName, addon = ...
 addon.Utilities = {}
 local Utilities = addon.Utilities
 
-function Utilities:Test(argument)
-	--
+function Utilities:GetTableKeys(input)
+	local keys = {}
+
+	for key, _ in pairs(input) do
+		table.insert(keys, tostring(key))
+	end
+
+	return unpack(keys)
+end
+
+function Utilities:PrintTableKeys(input, sorted)
+	local keys = { Utilities:GetTableKeys(input) }
+
+	if sorted ~= false then
+		table.sort(keys)
+	end
+
+	for _, key in ipairs(keys) do
+		print(key)
+	end
 end
