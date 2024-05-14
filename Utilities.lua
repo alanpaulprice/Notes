507e51b325fa4c50568c4d9029a1f6d104b2276b
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-05-14 17:22:22 +0100

    Utilities.lua: add CheckIsTable, GetTableLength, PrintMetaTableKeys, improve PrintTableKeys and GetTableKeys

--- Utilities.lua
+++ Utilities.lua
@@ -2,7 +2,27 @@ local addonName, addon = ...
 addon.Utilities = {}
 local Utilities = addon.Utilities
 
+function Utilities:CheckIsTable(input)
+	if type(input) ~= "table" then
+		error("Table expected, received " .. tostring(input) .. " (type: " .. type(input) .. ").")
+	end
+end
+
+function Utilities:GetTableLength(input)
+	Utilities:CheckIsTable(input)
+
+	local count = 0
+
+	for _ in pairs(input) do
+		count = count + 1
+	end
+
+	return count
+end
+
 function Utilities:GetTableKeys(input)
+	Utilities:CheckIsTable(input)
+
 	local keys = {}
 
 	for key, _ in pairs(input) do
@@ -12,14 +32,37 @@ function Utilities:GetTableKeys(input)
 	return unpack(keys)
 end
 
-function Utilities:PrintTableKeys(input, sorted)
-	local keys = { Utilities:GetTableKeys(input) }
+function Utilities:PrintMetaTableKeys(input, sorted)
+	Utilities:CheckIsTable(input)
 
-	if sorted ~= false then
-		table.sort(keys)
+	local metatable = getmetatable(input)
+
+	if Utilities:GetTableLength(metatable) > 0 then
+		Utilities:PrintTableKeys(metatable, sorted, false)
+	else
+		print("The metatable has no keys.")
+	end
+end
+
+function Utilities:PrintTableKeys(input, sorted, printMetatable)
+	Utilities:CheckIsTable(input)
+
+	if Utilities:GetTableLength(input) > 0 then
+		local keys = { Utilities:GetTableKeys(input) }
+
+		if sorted ~= false then
+			table.sort(keys)
+		end
+
+		for _, value in pairs(keys) do
+			print(value)
+		end
+	else
+		print("The table has no keys.")
 	end
 
-	for _, key in ipairs(keys) do
-		print(key)
+	if printMetatable ~= false then
+		print("-----")
+		Utilities:PrintMetaTableKeys(input, sorted)
 	end
 end
