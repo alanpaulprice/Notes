5d4f07e9984b89a7080b40b3bc50d419a6f58959
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-06-18 15:18:21 +0100

    Constants.lua: add `DEFAULT_UI_WIDTH` and `DEFAULT_UI_HEIGHT`

--- Constants.lua
+++ Constants.lua
@@ -2,6 +2,9 @@ local addonName, addon = ...
 addon.Constants = {}
 local Constants = addon.Constants
 
+Constants.DEFAULT_UI_WIDTH = 338
+Constants.DEFAULT_UI_HEIGHT = 424
+
 Constants.NOTE_TITLE_MAX_LENGTH = 32
 
 Constants.UI_VIEW_ENUM = {
