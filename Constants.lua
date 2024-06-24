a1ba3b83d0567291b84bb7aee6e5a4d67d2926ab
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-06-24 17:02:39 +0100

    Constants.lua: add properties: MIN_UI_WIDTH, MIN_UI_HEIGHT, MAX_UI_WIDTH, MAX_UI_HEIGHT

--- Constants.lua
+++ Constants.lua
@@ -5,6 +5,11 @@ local Constants = addon.Constants
 Constants.DEFAULT_UI_WIDTH = 338
 Constants.DEFAULT_UI_HEIGHT = 424
 
+Constants.MIN_UI_WIDTH = 200
+Constants.MIN_UI_HEIGHT = 200
+Constants.MAX_UI_WIDTH = nil
+Constants.MAX_UI_HEIGHT = nil
+
 Constants.NOTE_TITLE_MAX_LENGTH = 32
 
 Constants.UI_VIEW_ENUM = {
