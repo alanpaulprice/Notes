b64659a6872571c3e9322a1f00f510577b462d0c
Alan Price <23743415+alanpaulprice@users.noreply.github.com>
2024-11-07 12:24:09 +0000

    EditView.lua: rename `EditView.EditBox` to `EditView.editBox`

--- EditView.lua
+++ EditView.lua
@@ -6,43 +6,45 @@ local AceGUI = LibStub("AceGUI-3.0")
 
 local function UpdateEditBoxText()
 	local note = addon.Database:GetCurrentNote()
-	EditView.EditBox:SetText(note.body)
+	EditView.editBox:SetText(note.body)
 end
 
 function EditView:Build(container)
-	EditView.EditBox = AceGUI:Create("MultiLineEditBox")
-	EditView.EditBox:SetLabel("")
-	EditView.EditBox:DisableButton(true)
-	EditView.EditBox:SetFullHeight(true)
-	EditView.EditBox:SetFullWidth(true)
-	EditView.EditBox.editBox:SetTextInsets(8, 8, 8, 8)
-	local _, _, fontFlags = EditView.EditBox.editBox:GetFontObject():GetFont()
-	EditView.EditBox.editBox:SetFont(
+	EditView.editBox = AceGUI:Create("MultiLineEditBox")
+	EditView.editBox:SetLabel("")
+	EditView.editBox:DisableButton(true)
+	EditView.editBox:SetFullHeight(true)
+	EditView.editBox:SetFullWidth(true)
+	EditView.editBox.editBox:SetTextInsets(8, 8, 8, 8)
+	local _, _, fontFlags = EditView.editBox.editBox:GetFontObject():GetFont()
+	EditView.editBox.editBox:SetFont(
 		AceGUIWidgetLSMlists.font[addon.Database:GetEditViewFont()],
 		addon.Database:GetEditViewFontSize(),
 		fontFlags
 	)
-	EditView.EditBox:SetCallback("OnTextChanged", function(_, _, text)
+	EditView.editBox:SetCallback("OnTextChanged", function(_, _, text)
 		addon.Database:SetNoteBody(addon.Database:GetCurrentNoteId(), text)
 	end)
 	UpdateEditBoxText()
-	container:AddChild(EditView.EditBox)
+	container:AddChild(EditView.editBox)
 end
 
 function EditView:UpdateFontSize(fontSize)
 	addon.Database:SetEditViewFontSize(fontSize)
 
-	if self.EditBox and self.EditBox:IsShown() then
-		local fontName, _, fontFlags = EditView.EditBox.editBox:GetFontObject():GetFont()
-		EditView.EditBox.editBox:SetFont(fontName, fontSize, fontFlags)
+	if self.editBox and self.editBox:IsShown() then
+		local fontName, _, fontFlags = self.editBox.editBox:GetFontObject():GetFont()
+		self.editBox.editBox:SetFont(fontName, fontSize, fontFlags)
 	end
+
+	AceConfigRegistry:NotifyChange(addonName)
 end
 
 function EditView:UpdateFont(font)
 	addon.Database:SetEditViewFont(font)
 
-	if self.EditBox and self.EditBox:IsShown() then
-		local _, fontSize, fontFlags = EditView.EditBox.editBox:GetFontObject():GetFont()
-		EditView.EditBox.editBox:SetFont(AceGUIWidgetLSMlists.font[font], fontSize, fontFlags)
+	if self.editBox and self.editBox:IsShown() then
+		local _, fontSize, fontFlags = self.editBox.editBox:GetFontObject():GetFont()
+		self.editBox.editBox:SetFont(AceGUIWidgetLSMlists.font[font], fontSize, fontFlags)
 	end
 end
